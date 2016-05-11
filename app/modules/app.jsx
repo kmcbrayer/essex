7330fa1e12cc2f5656396a20d90506d35daf6c89
Kevin <kmcbrayer77@gmail.com>
2016-05-11 07:54:30 -0400

    SEED START, install react and react-dom and begin creating components

--- app/modules/app.jsx
+++ app/modules/app.jsx
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log("sup guys?")
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Test = React.createClass({
+    render: function () {
+        return (
+            <div>Hey dudes</div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <Test />,
+    document.getElementById('content')
+);
